a9efa2457a4d7d1ed2e28ef6178bdf1d87abfa9b
adityasawant <adityasawant.294@gmail.com>
2025-12-17 23:31:13 -0600

    codecrafters submit [skip ci]

--- app/shell.go
+++ app/shell.go
@@ -18,6 +18,7 @@ var builtinCmds = map[string]bool{
 }
 
 func execCommand(command string) error {
+	// command = `ls /tmp/dog | tail -n 5 | head -n 3 | grep "f-47"`
 	command = strings.TrimSpace(command)
 
 	if command == "" {
@@ -25,18 +26,340 @@ func execCommand(command string) error {
 	}
 
 	if strings.Contains(command, "|") {
+		// fmt.Fprintf(os.Stderr, "[PIPELINE]: %s\n", command)
 		return execPipeline(command)
 	}
-
-	return execSingleCommand(command, nil, os.Stdout, os.Stderr)
+	// fmt.Fprintf(os.Stderr, "[SINGLE COMMAND]: %s\n", command)
+	return execSingleCommand(command, os.Stdin, os.Stdout, os.Stderr)
 }
 
 func execPipeline(command string) error {
-	parts := strings.Split(command, "|")
-	if len(parts) != 2 {
-		return fmt.Errorf("only two-command pipelines are supported")
+
+	// if strings.Contains(command, "grep") {
+	// 	dump(command)
+	// }
+	commandParts := strings.Split(command, "|")
+
+	type Pipe struct {
+		reader *os.File
+		writer *os.File
 	}
 
+	type CommandInfo struct {
+		cmd       string
+		args      []string
+		isBuiltin bool
+		stdin     io.Reader
+		stdout    io.Writer
+		stderr    io.Writer
+	}
+
+	commands := make([]CommandInfo, 0)
+
+	for _, part := range commandParts {
+		cmd := strings.TrimSpace(part)
+		args, _, _ := parseArgString(cmd)
+
+		if len(args) == 0 {
+			continue
+		}
+
+		isBuiltin := builtinCmds[args[0]]
+		commands = append(commands, CommandInfo{cmd: cmd, args: args, isBuiltin: isBuiltin})
+	}
+
+	if len(commands) == 0 {
+		return nil
+	}
+
+	// dump(fmt.Sprintf("Inside Pipeline %d commands: %+v\n", len(commandParts), commands))
+
+	numPipes := len(commands) - 1
+	pipes := make([]Pipe, numPipes)
+
+	// create all pipes
+	for i := 0; i < numPipes; i++ {
+		reader, writer, err := os.Pipe()
+		if err != nil {
+			return fmt.Errorf("failed to create pipe %d: %w", i, err)
+		}
+		pipes[i] = Pipe{reader: reader, writer: writer}
+	}
+
+	// assign pipes to each command
+	for i := 0; i < len(commands); i++ {
+		if i == 0 {
+			commands[i].stdin = os.Stdin
+		} else {
+			commands[i].stdin = pipes[i-1].reader
+		}
+
+		if i == len(commands)-1 {
+			commands[i].stdout = os.Stdout
+		} else {
+			commands[i].stdout = pipes[i].writer
+		}
+		commands[i].stderr = os.Stderr
+	}
+
+	errChannels := make([]chan error, len(commands))
+	externalCmds := make([]*exec.Cmd, len(commands))
+
+	// start all commands
+	for i := 0; i < len(commands); i++ {
+		cmd := commands[i]
+		isLastCmd := i == len(commands)-1
+		if cmd.isBuiltin {
+			errChan := make(chan error, 1)
+			errChannels[i] = errChan
+
+			go func(c CommandInfo, ch chan error, isLast bool) {
+				defer func() {
+					if !isLast {
+						if closer, ok := c.stdout.(io.Closer); ok {
+							closer.Close()
+						}
+					}
+				}()
+
+				errChan <- execSingleCommand(
+					c.cmd,
+					c.stdin,
+					c.stdout,
+					c.stderr,
+				)
+			}(cmd, errChan, isLastCmd)
+
+		} else {
+			// fmt.Fprintf(os.Stderr, "Executing: %s\targs:%v\n", cmd.cmd, cmd.args)
+
+			if _, err := exec.LookPath(cmd.args[0]); err == nil {
+				// fmt.Fprintf(os.Stderr, "[DEBUG] PATH: %s\n", path)
+				execCmd := exec.Command(cmd.args[0], cmd.args[1:]...)
+
+				if i == 0 {
+					execCmd.Stdin = os.Stdin
+				} else {
+					execCmd.Stdin = cmd.stdin
+				}
+
+				if isLastCmd {
+					execCmd.Stdout = os.Stdout
+				} else {
+					execCmd.Stdout = cmd.stdout
+				}
+				execCmd.Stderr = cmd.stderr
+
+				// if err := execCmd.Start(); err != nil {
+				// 	// fmt.Fprintf(os.Stderr, "Failed to start %s: %v\n", cmd.args[0], err)
+				// 	return fmt.Errorf("failed to start %s: %w", cmd.args[0], err)
+				// }
+
+				// if !isLastCmd {
+				// 	if closer, ok := cmd.stdout.(io.Closer); ok {
+				// 		closer.Close()
+				// 	}
+				// }
+				externalCmds[i] = execCmd
+			} else {
+				// fmt.Printf("Executing: %s\n", parsedArgs[0])
+				fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd.args[0])
+			}
+		}
+	}
+
+	// start all external commands
+	for _, cmd := range externalCmds {
+		if cmd == nil {
+			continue
+		}
+		// fmt.Fprintf(os.Stderr, "Failed to start %s: %v\n", cmd.args[0], err)
+		// return fmt.Errorf("failed to start %s: %w", cmd.args[0], err)
+		cmd.Start()
+	}
+
+	// close all pipe writes in parent
+	for _, p := range pipes {
+		p.writer.Close()
+	}
+
+	var firstErr error
+
+	// wait for all builtin commands
+	for i := 0; i < len(commands); i++ {
+		if commands[i].isBuiltin {
+			if err := <-errChannels[i]; err != nil {
+				if firstErr == nil {
+					firstErr = err
+				}
+			}
+		}
+	}
+
+	// wait for all external commands
+	for _, cmd := range externalCmds {
+		if cmd == nil {
+			continue
+		}
+		if err := cmd.Wait(); err != nil {
+			if _, ok := err.(*exec.ExitError); ok {
+				continue
+			}
+			if firstErr == nil {
+				firstErr = err
+			}
+		}
+	}
+
+	for _, p := range pipes {
+		p.reader.Close()
+	}
+
+	// return firstErr
+	return nil
+}
+
+func execSingleCommand(command string, stdin io.Reader, stdout, stderr io.Writer) error {
+	command = strings.TrimSuffix(command, "\n")
+	parsedArgs, redirectFileName, redirectType := parseArgString(command)
+
+	var f *os.File
+	var err error
+	if redirectFileName != "" {
+		f, err = createFile(redirectFileName, redirectType)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+	}
+
+	w := stdout
+	e := stderr
+	if f != nil {
+		switch redirectType {
+		case RedirectStdout, RedirectAppend:
+			w = f
+		case RedirectStderr, RedirectStderrAppend:
+			e = f
+		}
+	}
+
+	if len(parsedArgs) == 0 {
+		return nil
+	}
+
+	switch parsedArgs[0] {
+	case "exit":
+		if len(parsedArgs) == 1 {
+			os.Exit(0)
+		}
+		code, err := strconv.Atoi(parsedArgs[1])
+		if err != nil {
+			return err
+		}
+		os.Exit(code)
+
+	case "echo":
+		outputStrings := strings.Join(parsedArgs[1:], " ")
+
+		fmt.Fprintln(w, outputStrings)
+
+	case "type":
+		if len(parsedArgs) < 2 {
+			fmt.Fprintln(e, "type: missing operand")
+			return nil
+		}
+
+		if ok := builtinCmds[parsedArgs[1]]; ok {
+			fmt.Fprintf(w, "%s is a shell builtin\n", parsedArgs[1])
+		} else {
+
+			cmd := parsedArgs[1]
+
+			path, err := exec.LookPath(cmd)
+
+			if err == nil {
+				fmt.Fprintf(w, "%s is %s\n", cmd, path)
+				return nil
+			} else {
+				fmt.Fprintf(e, "%s: not found\n", parsedArgs[1])
+			}
+
+		}
+
+	case "cat":
+		// fmt.Fprintf(os.Stderr, "Execution cat: %+v\n", parsedArgs)
+		for _, fname := range parsedArgs[1:] {
+			fHandle, err := os.Open(fname)
+			if err != nil {
+				fmt.Fprintf(e, "cat: %s: No such file or directory\n", fname)
+				continue
+			}
+
+			io.Copy(w, fHandle)
+			fHandle.Close()
+		}
+
+	case "pwd":
+		pwd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(w, "%s\n", pwd)
+
+	case "cd":
+
+		if len(parsedArgs) == 1 {
+			os.Chdir(os.Getenv("HOME"))
+			return nil
+		}
+
+		if parsedArgs[1] == "~" {
+			parsedArgs[1] = os.Getenv("HOME")
+		}
+
+		err := os.Chdir(parsedArgs[1])
+
+		if err != nil {
+			fmt.Fprintf(e, "cd: %s: No such file or directory\n", parsedArgs[1])
+		}
+	default:
+		// fmt.Fprintf(os.Stderr, "Execution External: %v", parsedArgs)
+		_, err := exec.LookPath(parsedArgs[0])
+		var command *exec.Cmd
+		if err == nil {
+
+			command = exec.Command(parsedArgs[0], parsedArgs[1:]...)
+			command.Stdin = stdin
+
+			if f != nil && (redirectType == RedirectStdout || redirectType == RedirectAppend) {
+				command.Stdout = f
+			} else {
+				command.Stdout = stdout
+			}
+
+			if f != nil && (redirectType == RedirectStderr || redirectType == RedirectStderrAppend) {
+				command.Stderr = f
+			} else {
+				command.Stderr = stderr
+			}
+
+			err := command.Run()
+			if _, ok := err.(*exec.ExitError); ok {
+				return nil
+			} else {
+				return err
+			}
+		} else {
+			// fmt.Printf("Executing: %s\n", parsedArgs[0])
+			fmt.Fprintf(e, "%s: command not found\n", parsedArgs[0])
+		}
+
+	}
+	return nil
+}
+
+/*
 	leftCmd := strings.TrimSpace(parts[0])
 	rightCmd := strings.TrimSpace(parts[1])
 
@@ -156,144 +479,4 @@ func execPipeline(command string) error {
 		//wait for right command to finish
 		return rightExec.Wait()
 	}
-
-}
-
-func execSingleCommand(command string, stdin io.Reader, stdout, stderr io.Writer) error {
-	command = strings.TrimSuffix(command, "\n")
-	parsedArgs, redirectFileName, redirectType := parseArgString(command)
-
-	var f *os.File
-	var err error
-	if redirectFileName != "" {
-		f, err = createFile(redirectFileName, redirectType)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-	}
-
-	w := stdout
-	e := stderr
-	if f != nil {
-		switch redirectType {
-		case RedirectStdout, RedirectAppend:
-			w = f
-		case RedirectStderr, RedirectStderrAppend:
-			e = f
-		}
-	}
-
-	if len(parsedArgs) == 0 {
-		return nil
-	}
-
-	switch parsedArgs[0] {
-	case "exit":
-		if len(parsedArgs) == 1 {
-			os.Exit(0)
-		}
-		code, err := strconv.Atoi(parsedArgs[1])
-		if err != nil {
-			return err
-		}
-		os.Exit(code)
-
-	case "echo":
-		outputStrings := strings.Join(parsedArgs[1:], " ")
-
-		fmt.Fprintln(w, outputStrings)
-
-	case "type":
-		if len(parsedArgs) < 2 {
-			fmt.Fprintln(e, "type: missing operand")
-			return nil
-		}
-
-		if ok := builtinCmds[parsedArgs[1]]; ok {
-			fmt.Fprintf(w, "%s is a shell builtin\n", parsedArgs[1])
-		} else {
-
-			cmd := parsedArgs[1]
-
-			path, err := exec.LookPath(cmd)
-
-			if err == nil {
-				fmt.Fprintf(w, "%s is %s\n", cmd, path)
-				return nil
-			} else {
-				fmt.Fprintf(e, "%s: not found\n", parsedArgs[1])
-			}
-
-		}
-
-	case "cat":
-
-		for _, fname := range parsedArgs[1:] {
-			fHandle, err := os.Open(fname)
-			if err != nil {
-				fmt.Fprintf(e, "cat: %s: No such file or directory\n", fname)
-				continue
-			}
-
-			io.Copy(w, fHandle)
-			fHandle.Close()
-		}
-
-	case "pwd":
-		pwd, err := os.Getwd()
-		if err != nil {
-			return err
-		}
-		fmt.Fprintf(w, "%s\n", pwd)
-
-	case "cd":
-
-		if len(parsedArgs) == 1 {
-			os.Chdir(os.Getenv("HOME"))
-			return nil
-		}
-
-		if parsedArgs[1] == "~" {
-			parsedArgs[1] = os.Getenv("HOME")
-		}
-
-		err := os.Chdir(parsedArgs[1])
-
-		if err != nil {
-			fmt.Fprintf(e, "cd: %s: No such file or directory\n", parsedArgs[1])
-		}
-	default:
-
-		_, err := exec.LookPath(parsedArgs[0])
-		var command *exec.Cmd
-		if err == nil {
-
-			command = exec.Command(parsedArgs[0], parsedArgs[1:]...)
-
-			if f != nil && (redirectType == RedirectStdout || redirectType == RedirectAppend) {
-				command.Stdout = f
-			} else {
-				command.Stdout = os.Stdout
-			}
-
-			if f != nil && (redirectType == RedirectStderr || redirectType == RedirectStderrAppend) {
-				command.Stderr = f
-			} else {
-				command.Stderr = os.Stderr
-			}
-
-			err := command.Run()
-			if _, ok := err.(*exec.ExitError); ok {
-				return nil
-			} else {
-				return err
-			}
-		} else {
-			// fmt.Printf("Executing: %s\n", parsedArgs[0])
-			fmt.Fprintf(e, "%s: command not found\n", parsedArgs[0])
-		}
-
-	}
-	return nil
-}
+*/
--- app/utils.go
+++ app/utils.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -47,4 +50,62 @@ func findLongestCommonPrefix(matches []string) string {
 		}
 	}
 	return longestCommonPrefix
+}
+
+func dump(command string) {
+	fmt.Fprintf(os.Stderr, "\n=== DEBUG PIPELINE ===\n")
+	fmt.Fprintf(os.Stderr, "Command: %s\n\n", command)
+
+	// Test each stage manually
+	commandParts := strings.Split(command, "|")
+	var lastOutput []byte
+
+	for i, part := range commandParts {
+		cmdStr := strings.TrimSpace(part)
+		args, _, _ := parseArgString(cmdStr)
+
+		if len(args) == 0 {
+			continue
+		}
+
+		fmt.Fprintf(os.Stderr, "--- Stage %d: %s ---\n", i+1, cmdStr)
+
+		cmd := exec.Command(args[0], args[1:]...)
+
+		if i > 0 {
+			cmd.Stdin = bytes.NewReader(lastOutput)
+		}
+
+		output, err := cmd.Output()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				fmt.Fprintf(os.Stderr, "Exit code: %d\n", exitErr.ExitCode())
+			}
+		}
+
+		lines := bytes.Count(output, []byte("\n"))
+		if len(output) > 0 && output[len(output)-1] != '\n' {
+			lines++ // Count last line if no trailing newline
+		}
+
+		fmt.Fprintf(os.Stderr, "Output: %d bytes, %d lines\n", len(output), lines)
+
+		if len(output) > 0 {
+			scanner := bufio.NewScanner(bytes.NewReader(output))
+			lineNum := 1
+			for scanner.Scan() {
+				fmt.Fprintf(os.Stderr, "  %d: %q\n", lineNum, scanner.Text())
+				lineNum++
+			}
+		} else {
+			fmt.Fprintf(os.Stderr, "  (empty)\n")
+		}
+
+		fmt.Fprintf(os.Stderr, "\n")
+		lastOutput = output
+	}
+
+	fmt.Fprintf(os.Stderr, "=== END DEBUG ===\n\n")
 }
